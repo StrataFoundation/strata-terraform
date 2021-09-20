58faa6f705a98b8d7306de29d74cb09b2a8c8287
Chewing Glass <chewinglass123@gmail.com>
2021-09-20 13:02:08 -0500

    Try private subnets again

--- redis-commander.tf
+++ redis-commander.tf
@@ -7,7 +7,7 @@ module "rediscommander" {
   zone_id = var.zone_id
   lb_security_groups = [data.aws_security_group.default.id, aws_security_group.allow_http_https_inbound.id]
   service_security_groups =  [data.aws_security_group.default.id, module.web_server_sg.security_group_id]
-  subnets = module.vpc.public_subnets
+  subnets = module.vpc.private_subnets
   vpc_id = module.vpc.vpc_id
   certificate_arn = aws_acm_certificate.team_wumbo.arn
   cpu = 256
