74994fa8632b23cf676ce3cb291ed823489aed66
Chewing Glass <chewinglass123@gmail.com>
2021-09-23 20:33:38 -0500

    Put kafka in public subnet, I want to hit it

--- kafka.tf
+++ kafka.tf
@@ -23,7 +23,7 @@ resource "aws_msk_cluster" "kafka" {
   broker_node_group_info {
     instance_type   = var.kafka_instance_type
     ebs_volume_size = var.kafka_ebs_size
-    client_subnets = module.vpc.private_subnets
+    client_subnets = module.vpc.public_subnets
     security_groups = [data.aws_security_group.default.id]
   }
 
