1e822ef389b575ffe4d670e803bc45979d458df7
Chewing Glass <chewinglass123@gmail.com>
2021-10-01 09:26:14 -0700

    Counts changes ignored

--- modules/service_with_lb/main.tf
+++ modules/service_with_lb/main.tf
@@ -224,7 +224,6 @@ resource "aws_ecs_service" "service" {
 
   lifecycle {
     ignore_changes = [
-      desired_count,
       health_check_grace_period_seconds,
       capacity_provider_strategy,
       deployment_circuit_breaker,
