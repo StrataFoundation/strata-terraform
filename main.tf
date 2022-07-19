728aab435a59997120b95ce07364bc06fd07eb55
Chewing Glass <chewinglass123@gmail.com>
2022-07-19 12:41:53 -0500

    public database group

--- main.tf
+++ main.tf
@@ -30,6 +30,13 @@ module "vpc" {
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
+
+  create_database_subnet_group           = true
+  create_database_subnet_route_table     = true
+  create_database_internet_gateway_route = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 module "vpn" {
--- rds.tf
+++ rds.tf
@@ -1,4 +1,4 @@
-module "security_group" {
+module "vybe_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
 
@@ -67,7 +67,7 @@ resource "aws_db_instance" "default" {
 
   multi_az               = true
   db_subnet_group_name   = module.vpc.database_subnet_group
-  vpc_security_group_ids = [module.vpc_security_group.security_group_id, module.security_group.security_group_id]
+  vpc_security_group_ids = [module.vpc_security_group.security_group_id, module.vybe_security_group.security_group_id]
 
   maintenance_window              = "Mon:00:00-Mon:03:00"
   backup_window                   = "03:00-06:00"
