5c8723fe284822265c5f24293689d767a1a6c09e
Chewing Glass <chewinglass123@gmail.com>
2022-07-19 18:47:22 -0500

    try sth new

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ module "vpc" {
   azs             = var.aws_azs
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
+  database_subnets = var.database_subnets
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
--- variables.tf
+++ variables.tf
@@ -36,6 +36,11 @@ variable "public_subnets" {
   default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
+variable "database_subnets" {
+  type = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
 variable "num_kafka_nodes" {
   type = number
   default = 3
