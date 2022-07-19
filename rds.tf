4a9d6f6fdde795d8ffbaa13816648bb1befea320
Chewing Glass <chewinglass123@gmail.com>
2022-07-19 18:37:07 -0500

    Use subnet group name

--- rds.tf
+++ rds.tf
@@ -66,7 +66,7 @@ resource "aws_db_instance" "default" {
   port     = 5432
 
   multi_az               = true
-  db_subnet_group_name   = module.vpc.database_subnet_group
+  db_subnet_group_name   = module.vpc.database_subnet_group_name
   vpc_security_group_ids = [module.vpc_security_group.security_group_id, module.vybe_security_group.security_group_id]
 
   maintenance_window              = "Mon:00:00-Mon:03:00"
