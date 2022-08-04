54bf5cb742bd14e750a2f305ae60a69535461f42
Chewing Glass <chewinglass123@gmail.com>
2022-08-04 11:51:22 -0500

    More conf hell

--- rabbit.tf
+++ rabbit.tf
@@ -1,25 +1,5 @@
-resource "aws_mq_configuration" "rabbitmq" {
-  description    = "RabbitMQ Config"
-  name           = "rabbitmq"
-  engine_type    = "RabbitMQ"
-  engine_version = "5.15.9"
-
-    data = <<DATA
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<broker xmlns="http://activemq.apache.org/schema/core">
-  <plugins>
-  </plugins>
-</broker>
-DATA
-}
-
 resource "aws_mq_broker" "rabbitmq" {
   broker_name = "rabbitmq"
-
-  configuration {
-    id       = aws_mq_configuration.rabbitmq.id
-    revision = aws_mq_configuration.rabbitmq.latest_revision
-  }
 
   engine_type        = "RabbitMQ"
   engine_version     = "5.15.9"
