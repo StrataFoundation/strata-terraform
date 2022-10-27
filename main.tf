0df38d7bb9d10c00ea8fae3a1034dff098c2f8d7
Chewing Glass <chewinglass123@gmail.com>
2022-10-27 16:28:38 -0500

    Add token

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ terraform {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 provider "aws" {
