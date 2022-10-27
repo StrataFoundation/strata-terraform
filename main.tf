28f05ccc6f680d2073c54f09824aec043606c4ef
Chewing Glass <chewinglass123@gmail.com>
2022-10-27 14:02:06 -0500

    Add argo

--- main.tf
+++ main.tf
@@ -26,6 +26,13 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)  
+  }
+}
+
 
 provider "aws" {
   region = var.aws_region
