c3b99f649673bd5144014f2c95430708517532c9
Chewing Glass <chewinglass123@gmail.com>
2022-10-27 15:32:44 -0500

    Fix version

--- external_dns.tf
+++ external_dns.tf
@@ -1,5 +1,6 @@
 module "external_dns_helm" {
-  source = "lablabs/terraform-aws-eks-external-dns"
+  source  = "lablabs/eks-external-dns/aws"
+  version = "1.1.0"
 
   enabled           = true
   argo_enabled      = false
@@ -9,7 +10,7 @@ module "external_dns_helm" {
   cluster_identity_oidc_issuer_arn = module.eks_cluster.eks_cluster_identity_oidc_issuer_arn
 
   policy_allowed_zone_ids = var.zone_ids
-  
+
   helm_release_name = "aws-ext-dns-helm"
   namespace         = "aws-external-dns-helm"
 }
