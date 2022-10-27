57dd311efc6d748553c2f90c01cd343253fdf969
Chewing Glass <chewinglass123@gmail.com>
2022-10-27 12:30:31 -0500

    Add k8s

--- main.tf
+++ main.tf
@@ -4,8 +4,25 @@ terraform {
       source = "hashicorp/aws"
       version = "4.36.1"
     }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.14.0"
+    }
   }
 }
+
+
+
+# Kubernetes provider
+# https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
+# To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
+# The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
+# You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
 
 provider "aws" {
   region = var.aws_region
