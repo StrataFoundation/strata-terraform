f1c2b923a3679b9e3af13629fd677a4b28271140
Chewing Glass <chewinglass123@gmail.com>
2022-10-28 17:42:55 -0500

    Use ingress nginx to deploy argo

--- argo.tf
+++ argo.tf
@@ -6,6 +6,26 @@ resource "helm_release" "argocd" {
   namespace        = "argocd"
   version          = "5.8.3"
   create_namespace = true
+
+  set {
+    name = "server.insecure"
+    value = "true"
+  }
+
+  set {
+    name = "server.ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name = "server.ingress.hosts[0]"
+    value = "argocd.test-helium.com"
+  }
+
+  set {
+    name = "server.ingress.ingressClassName"
+    value = "nginx"
+  }
 }
 
 data "kubectl_path_documents" "application" {
