5d7284c42152e82190705ddf4449f6714ebb380f
Chewing Glass <chewinglass123@gmail.com>
2022-10-28 14:21:23 -0500

    nginx

--- lb.tf
+++ lb.tf
@@ -73,7 +73,7 @@ data "kubectl_path_documents" "nginx" {
     }
 }
 
-resource "kubectl_manifest" "argocd" {
+resource "kubectl_manifest" "nginx" {
   depends_on = [helm_release.argocd]
   count      = length(data.kubectl_path_documents.application.documents)
   yaml_body  = element(data.kubectl_path_documents.application.documents, count.index)
