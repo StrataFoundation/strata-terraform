dd709bef59e5cab78fc72d76ae7de1e886c661d8
Darwin Rinderer <darwin@helium.foundation>
2022-12-02 23:16:11 -0500

    Update k8s provider def

--- main.tf
+++ main.tf
@@ -29,7 +29,6 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.eks.token
-  load_config_file = false
 }
 
 provider "aws" {
