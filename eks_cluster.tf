f9a6265a7c6b0a59899ae6cf141ba0294c8d7889
Darwin Rinderer <darwin@helium.foundation>
2022-12-02 22:07:56 -0500

    is it the azs?

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -56,7 +56,6 @@ provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.eks.token
-  load_config_file = false
 }
 
 
--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.eks.token
+  load_config_file = false
 }
 
 provider "aws" {
