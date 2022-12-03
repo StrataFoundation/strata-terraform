19dfc12c42537074d179b6df0497c1b421f146f6
Darwin Rinderer <darwin@helium.foundation>
2022-12-03 00:33:58 -0500

    Revert "is it the azs?"
    This reverts commit f9a6265a7c6b0a59899ae6cf141ba0294c8d7889.

--- variables.tf
+++ variables.tf
@@ -18,22 +18,22 @@ variable "cidr_block" {
 
 variable "aws_azs" {
   type = list(string)
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "private_subnets" {
-  type = list(string)
-  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "public_subnets" {
   type = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type = list(string)
+  default = ["10.0.101.0/24", "10.0.102.0/24"]
 }
 
 variable "database_subnets" {
   type = list(string)
-  default = ["10.0.201.0/24", "10.0.202.0/24", "10.0.203.0/24"]
+  default = ["10.0.201.0/24", "10.0.202.0/24"]
 }
 
 variable "instance_type" {
