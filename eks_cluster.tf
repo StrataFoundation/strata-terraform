f4de44f8fec06d3afeb3f025e36ca5fe03e5daaf
Darwin Rinderer <darwin@helium.foundation>
2022-12-14 17:06:13 -0500

    Removing unnecessary sg rule

--- eks_cluster.tf
+++ eks_cluster.tf
@@ -24,19 +24,6 @@ module "eks" {
     "kubernetes.io/cluster/${local.cluster_name}" = null
   }
 
-  # This rule on the node security group appears to be required to let pods
-  # with the rds-access-security-group sg communicate with the open internet
-  node_security_group_additional_rules  = {
-    ingress_allow_access_fron_rds_access_sg = {
-      type                          = "ingress"
-      from_port                     = 0
-      to_port                       = 0
-      protocol                      = "-1"
-      source_security_group_id      = aws_security_group.rds_access_security_group.id
-      description                   = "Allow access from rds-access-security-group"
-    }
-  }
-
   eks_managed_node_groups = {
     medium_group = {
       name = "small-node-group"
