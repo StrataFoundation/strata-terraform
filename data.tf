9630432740d72b3d4df7093d0a8fec4f0333aa69
Darwin Rinderer <darwin@helium.foundation>
2022-12-15 13:09:20 -0500

    Updating eks auth configmap to allow kubectl access to AWSAdministratorAccess IAM role

--- data.tf
+++ data.tf
@@ -28,3 +28,8 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
+data "aws_iam_roles" "admin_role" {
+  name_regex  = "AWSReservedSSO_AWSAdministratorAccess_.*"
+  path_prefix = "/aws-reserved/sso.amazonaws.com/"
+}
