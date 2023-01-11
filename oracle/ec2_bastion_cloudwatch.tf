1a776f58702dbe6f854cf35f4827d24203abfffb
Darwin Rinderer <darwin@helium.foundation>
2023-01-11 10:40:42 -0500

    Adding log group and addressing typo

--- oracle/ec2_bastion_cloudwatch.tf
+++ oracle/ec2_bastion_cloudwatch.tf
@@ -1,8 +1,14 @@
+# Add CloudWatch log group for ssh access logs from bastion
+resource "aws_cloudwatch_log_group" "bastion_ssh_denied_log_group" {
+  name              = "/aws/ec2/bastion/ssh"
+  retention_in_days = 120
+}
+
 # Add CloudWatch metrics filter to ssh access logs pulled shipped from Bastion to isolate failed login attempts as a metric 
 resource "aws_cloudwatch_log_metric_filter" "bastion_ssh_metrics_filter" {
   name           = "bastion-ssh-metrics-filter"
   pattern        = "failed, status 22"
-  log_group_name = "/aws/ec2/bastion/ssh"
+  log_group_name = aws_cloudwatch_log_group.bastion_ssh_denied_log_group.name
 
   metric_transformation {
     name         = "ssh-denied"
@@ -12,7 +18,7 @@ resource "aws_cloudwatch_log_metric_filter" "bastion_ssh_metrics_filter" {
 }
 
 # CloudWath alert on failed login attempts to bastion
-resource "aws_cloudwatch_metric_alarm" "bastion_ssh_denied" {
+resource "aws_cloudwatch_metric_alarm" "bastion_ssh_denied_alarm" {
   alarm_name          = "bastion-ssh-denied"
   metric_name         = "ssh-denied"
   threshold           = "0"
--- oracle/ec2_bastion_iam.tf
+++ oracle/ec2_bastion_iam.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "bastion_cw_agent_role" {
   name        = "bastion-cloudwatch-agent-role"
   description = "EC2 IAM Instance Profile to facilitate logging by CloudWatch Agent"
 
-  managed_managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"]
+  managed_policy_arns = ["arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"]
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
