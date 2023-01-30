91f8345bc0b8c79fcf3d4572c3af515f4fc169b8
Darwin Rinderer <darwin@helium.foundation>
2023-01-30 17:56:04 -0500

    Cleaning up oracle

--- modules/eks_oracle/outputs.tf
+++ modules/eks_oracle/outputs.tf
@@ -5,3 +5,15 @@ output "oidc_provider" {
 output "oidc_provider_arn" {
   value = module.eks.oidc_provider_arn
 }
+
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  value = module.eks.cluster_security_group_id
+}
--- oracle/outputs.tf
+++ oracle/outputs.tf
@@ -1,16 +1,16 @@
 output "cluster_id" {
   description = "EKS cluster ID"
-  value       = module.eks.cluster_id
+  value       = module.eks_oracle.cluster_id
 }
 
 output "cluster_endpoint" {
   description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+  value       = module.eks_oracle.cluster_endpoint
 }
 
 output "cluster_security_group_id" {
   description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
+  value       = module.eks_oracle.cluster_security_group_id
 }
 
 output "region" {
@@ -20,10 +20,10 @@ output "region" {
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
-  value       = local.cluster_name
+  value       = var.cluster_name
 }
 
 output "bastion_eip" {
   description = "Bastion EIP (static public IP address)"
-  value       = aws_eip.bastion_eip.public_ip
+  value       = module.bastion.bastion_eip
 }
