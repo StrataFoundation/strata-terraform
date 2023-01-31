5408a3996af72658d1f11d48ef1197109683c0ab
Darwin Rinderer <darwin@helium.foundation>
2023-01-31 14:07:58 -0500

    Addressing eks outputs

--- modules/eks_oracle/outputs.tf
+++ modules/eks_oracle/outputs.tf
@@ -17,3 +17,7 @@ output "cluster_endpoint" {
 output "cluster_security_group_id" {
   value = module.eks.cluster_security_group_id
 }
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
