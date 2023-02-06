a1511ffe73a9b3431ae47b1aaa2c8cafc8a777cc
Chewing Glass <chewinglass123@gmail.com>
2023-02-06 14:07:26 -0600

    Fix security group for rds

--- web-k8s/main.tf
+++ web-k8s/main.tf
@@ -72,3 +72,21 @@ resource "kubernetes_service_account" "rds_web_access" {
     }
   }
 }
+
+resource "kubectl_manifest" "rds-access-security-group-policy" {
+    yaml_body = <<YAML
+apiVersion: vpcresources.k8s.aws/v1beta1
+kind: SecurityGroupPolicy
+metadata:
+  name: rds-access-security-group-policy
+  namespace: helium
+spec:
+  podSelector: 
+    matchLabels: 
+      security-group: rds-access
+  securityGroups:
+    groupIds:
+      - "${data.aws_security_group.rds_access_security_group.id}"
+      - "${data.aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id}"
+YAML
+}
