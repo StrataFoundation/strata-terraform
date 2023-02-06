b3b1f4e8a5625fcbe2829e5a34bf12e7309ec55f
Chewing Glass <chewinglass123@gmail.com>
2023-02-06 15:16:48 -0600

    rename manifest

--- modules/k8s/lb.tf
+++ modules/k8s/lb.tf
@@ -50,7 +50,7 @@ resource "helm_release" "lbc" {
   }
 }
 
-resource "kubectl_manifest" "nginx" {
+resource "kubectl_manifest" "ingress-nginx" {
   depends_on = [helm_release.lbc]
   count      = length(data.kubectl_path_documents.nginx.documents)
   yaml_body  = element(data.kubectl_path_documents.nginx.documents, count.index)
