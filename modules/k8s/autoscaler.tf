b576d1a28d59a6d458eab28ffa99d9251705d515
Darwin Rinderer <darwin@helium.foundation>
2023-03-15 15:21:36 -0400

    Updates based on testing

--- modules/k8s/autoscaler.tf
+++ modules/k8s/autoscaler.tf
@@ -1,5 +1,5 @@
 resource "kubectl_manifest" "autoscaler" {
-  count      = var. with_autoscaler ? length(data.kubectl_path_documents.autoscaler.documents) : 0
-  
+  count      = var.with_autoscaler ? length(data.kubectl_path_documents.autoscaler.documents) : 0
+
   yaml_body  = element(data.kubectl_path_documents.autoscaler.documents, count.index)
 }
