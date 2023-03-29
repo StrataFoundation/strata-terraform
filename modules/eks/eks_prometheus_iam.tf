1ebb6c3a0a0358f96886b0e915caf906c0b12a9c
Darwin Rinderer <darwin@helium.foundation>
2023-03-29 14:33:34 -0400

    Decomposing inline IAM policy definition

--- modules/eks/eks_prometheus_iam.tf
+++ modules/eks/eks_prometheus_iam.tf
@@ -4,24 +4,6 @@ resource "aws_iam_role" "prometheus_write_access" {
   name        = "EKS-AMP-ServiceAccount-Role"
   description = "IAM role to be used by a K8s service account to assume cross account role"
 
-  inline_policy {
-    name   = "monitoring-role-assumption-policy" 
-    policy = jsonencode({
-      Version   = "2012-10-17"
-      Statement = [
-        {
-          Action   = [
-            "sts:AssumeRole"
-          ]
-          Effect   = "Allow"
-          Resource = [
-            "arn:aws:iam::${var.monitoring_account_id}:role/EKS-AMP-Central-Role"
-          ]
-        },
-      ]
-    })
-  }
-
   assume_role_policy = jsonencode({
     Version   = "2012-10-17"
     Statement = [
@@ -40,4 +22,29 @@ resource "aws_iam_role" "prometheus_write_access" {
       },
     ]
   })
+}
+
+resource "aws_iam_policy" "monitoring_role_assumption_policy" {
+  count = var.monitoring_account_id != "" ? 1 : 0
+
+  name   = "monitoring-role-assumption-policy" 
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action   = [
+          "sts:AssumeRole"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:iam::${var.monitoring_account_id}:role/EKS-AMP-Central-Role"
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "prometheus_required_iam_policy_attachment" {
+  role       = aws_iam_role.prometheus_write_access.id
+  policy_arn = data.aws_iam_policy.monitoring_role_assumption_policy.arn
 }
