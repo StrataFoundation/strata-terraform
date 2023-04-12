6982de5518fb3539de4be0805bc149c35b9687fa
Darwin Rinderer <darwin@helium.foundation>
2023-04-12 08:35:56 -0400

    Adding public monitoring RDS

---
+++ modules/rds-public/rds_security_group.tf
@@ -0,0 +1,67 @@
+# ***************************************
+# Security Group
+# RDS access security group
+# ***************************************
+resource "aws_security_group" "public_rds_access_security_group" {
+  name        = "public-rds-access-security-group"
+  description = "Security group required to access Public RDS instance"
+  vpc_id      = var.vpc_id
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "public-rds-access-security-group"
+  }
+}
+
+# ***************************************
+# Security Group
+# RDS security group
+# ***************************************
+resource "aws_security_group" "public_rds_security_group" {
+  name        = "public-rds-security-group"
+  description = "Security group for Public RDS resource"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "public-rds-security-group"
+  }
+}
+
+# ***************************************
+# Security Group Rules
+# for RDS security group
+# ***************************************
+resource "aws_security_group_rule" "public_rds_security_group_ingress_rule_1" {
+  type                     = "ingress"
+  description              = "Allow access from public-rds-access-security-group"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.public_rds_access_security_group.id
+  security_group_id        = aws_security_group.public_rds_security_group.id
+}
+
+resource "aws_security_group_rule" "public_rds_security_group_ingress_rule_3" {
+  type              = "ingress"
+  description       = "Allow access from Grafana" 
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = ["151.148.33.0/32", "151.148.33.2/32", "151.148.33.6/32"]
+  security_group_id = aws_security_group.public_rds_security_group.id
+}
+
+resource "aws_security_group_rule" "public_rds_security_group_egress_rule" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.public_rds_security_group.id
+}
---
+++ modules/rds-public/rds_user_iam.tf
@@ -0,0 +1,46 @@
+# ***************************************
+# IAM Role
+#
+# Public Monitoring RDS access
+# ***************************************
+resource "aws_iam_role" "rds_foundation_user_access_role" {
+  name        = "public-monitoring-rds-access-role"
+  description = "IAM Role for a K8s pod to assume to access the public monitoring RDS via the monitoring user"
+
+  inline_policy {
+    name   = "public-monitoring-rds-access-policy"
+    policy = jsonencode({
+      Version   = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "rds-db:connect"
+          ]
+          Effect   = "Allow"
+          Resource = [
+            "arn:aws:rds-db:${var.aws_region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.public_monitoring_rds.resource_id}/monitoring"
+          ]
+        },
+      ]
+    })
+  }
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = "${var.oidc_provider_arn}"
+        }
+        Condition = {
+          StringEquals = {
+            "${var.oidc_provider}:sub" = "system:serviceaccount:helium:public-monitoring-rds-monitoring-user-access"
+          }
+        }
+      },
+    ]
+  })
+}
