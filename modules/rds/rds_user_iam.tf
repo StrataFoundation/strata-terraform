c7fd7a5e3f38254a8ce7a6f69aea88b0fb04eb92
Darwin Rinderer <darwin@helium.foundation>
2023-05-15 11:14:06 -0400

    Updating conditional application of users

--- modules/rds/rds_user_iam.tf
+++ modules/rds/rds_user_iam.tf
@@ -7,6 +7,18 @@
 # IoT and Mobile AWS accounts can assume these roles in order to access the postgres db as the db-defined 
 # nova_<iot/mobile> users.
 # ***************************************
+locals {
+  rds_users_with_rr = {
+    true = [
+      "arn:aws:rds-db:${var.aws_region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.oracle_rds.resource_id}/${each.value.user}",
+      "arn:aws:rds-db:${var.aws_region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.oracle_rds_read_replica[0].resource_id}/${each.value.user}"
+    ]
+    false = [
+      "arn:aws:rds-db:${var.aws_region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.oracle_rds.resource_id}/${each.value.user}"
+    ]
+  }
+}
+
 resource "aws_iam_role" "rds_nova_user_access_role" {
   for_each = var.create_nova_dependent_resources ? local.nova.users : {}
 
@@ -23,16 +35,7 @@ resource "aws_iam_role" "rds_nova_user_access_role" {
             "rds-db:connect"
           ]
           Effect   = "Allow"
-          Resource = var.rds_read_replica 
-          ? 
-          [
-            "arn:aws:rds-db:${var.aws_region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.oracle_rds.resource_id}/${each.value.user}",
-            "arn:aws:rds-db:${var.aws_region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.oracle_rds_read_replica[0].resource_id}/${each.value.user}"
-          ]
-          :
-          [
-            "arn:aws:rds-db:${var.aws_region}:${data.aws_caller_identity.current.account_id}:dbuser:${aws_db_instance.oracle_rds.resource_id}/${each.value.user}"
-          ]
+          Resource = local.rds_users_with_rr[var.rds_read_replica]
         },
       ]
     })
