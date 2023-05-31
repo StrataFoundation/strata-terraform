ce19fd3692aa7bf46052578c38b7fe5699b1d5e8
Darwin Rinderer <darwin@helium.foundation>
2023-05-31 17:13:53 -0400

    Updating version combination

--- oracle/versions.tf
+++ oracle/versions.tf
@@ -1,8 +1,16 @@
 terraform {
+  cloud {
+    organization = "helium-foundation"
+
+    workspaces {
+      name = "oracle-sdlc-aws"
+    }
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "<= 5.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
--- poc-data/main.tf
+++ poc-data/main.tf
@@ -35,7 +35,7 @@ module "vpc" {
 # ***************************************
 module "notify_slack" {
   source  = "terraform-aws-modules/notify-slack/aws"
-  version = "~> 5.0"
+  version = "<= 5.0"
 
   sns_topic_name = "slack-topic"
 
