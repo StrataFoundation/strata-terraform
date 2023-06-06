e21553d0c45a6537b69ea9afb36235bd77ac82a3
Darwin Rinderer <darwin@helium.foundation>
2023-06-06 11:34:00 -0400

    Adding k8s provider to poc-data

--- poc-data/main.tf
+++ poc-data/main.tf
@@ -9,6 +9,17 @@ provider "aws" {
   }
 }
 
+# Kubernetes provider
+# https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
+# To learn how to schedule deployments and services using the provider, go here: https://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes
+# The Kubernetes provider is included in this file so the EKS module can complete successfully. Otherwise, it throws an error when creating `kubernetes_config_map.aws_auth`.
+# You should **not** schedule deployments and services in this workspace. This keeps workspaces modular (one for provision EKS, another for scheduling Kubernetes resources) as per best practices.
+provider "kubernetes" {
+  host                   = try(module.eks[0].cluster_endpoint, null)
+  cluster_ca_certificate = try(base64decode(module.eks[0].cluster_certificate_authority_data), null)
+  token                  = try(module.eks[0].aws_eks_cluster_auth, null)
+}
+
 # ***************************************
 # VPC
 # ***************************************
