5d45a4f8a624244349b44d35f2374608d7eaf674
Darwin Rinderer <darwin@helium.foundation>
2023-06-12 12:56:17 -0400

    Adding IAM policy for ECR access

---
+++ poc-data/ecr_iam.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_policy" "ecr_access_iam_policy" {
+  name   = "ECR-access-iam-policy"
+
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action   = [
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+          "ecr:UploadLayerPart",
+          "ecr:InitiateLayerUpload",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr-public:CompleteLayerUpload",
+          "ecr-public:GetAuthorizationToken",
+          "ecr-public:UploadLayerPart",
+          "ecr-public:InitiateLayerUpload",
+          "ecr-public:BatchCheckLayerAvailability",
+          "ecr-public:PutImage",
+          "sts:GetServiceBearerToken"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      },
+    ]
+  })
+}
