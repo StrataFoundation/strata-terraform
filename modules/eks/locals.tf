96c007d2d16a0a479bfd0affa67d842338a95072
Darwin Rinderer <darwin@helium.foundation>
2023-06-29 13:36:30 -0400

    Re-adding iam_role_additional_policies to node group

--- modules/eks/locals.tf
+++ modules/eks/locals.tf
@@ -2,47 +2,58 @@ locals {
   node_types = {
     poc-data = {
       medium_group = {
-        name                   = var.cluster_node_name
-        instance_types         = [var.eks_instance_type]
-        min_size               = var.cluster_min_size
-        max_size               = var.cluster_max_size
-        desired_size           = var.cluster_desired_size
-        vpc_security_group_ids = [
+        name                         = var.cluster_node_name
+        instance_types               = [var.eks_instance_type]
+        min_size                     = var.cluster_min_size
+        max_size                     = var.cluster_max_size
+        desired_size                 = var.cluster_desired_size
+        vpc_security_group_ids       = [
           aws_security_group.small_node_group.id
+        ]
+        iam_role_additional_policies = [
+        "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy" 
         ]
       }
       job_group = {
-        name                   = var.cluster_job_node_name
-        instance_types         = [var.eks_job_instance_type]
-        min_size               = var.cluster_job_min_size
-        max_size               = var.cluster_job_max_size
-        desired_size           = var.cluster_job_desired_size
-        vpc_security_group_ids = [
+        name                         = var.cluster_job_node_name
+        instance_types               = [var.eks_job_instance_type]
+        min_size                     = var.cluster_job_min_size
+        max_size                     = var.cluster_job_max_size
+        desired_size                 = var.cluster_job_desired_size
+        vpc_security_group_ids       = [
           aws_security_group.small_node_group.id
+        ]
+        iam_role_additional_policies = [
+        "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy" 
         ]
       }
     }
     oracle = {
       medium_group = {
-        name                   = var.cluster_node_name
-        instance_types         = [var.eks_instance_type]
-        min_size               = var.cluster_min_size
-        max_size               = var.cluster_max_size
-        desired_size           = var.cluster_desired_size
-        vpc_security_group_ids = [
+        name                         = var.cluster_node_name
+        instance_types               = [var.eks_instance_type]
+        min_size                     = var.cluster_min_size
+        max_size                     = var.cluster_max_size
+        desired_size                 = var.cluster_desired_size
+        vpc_security_group_ids       = [
           aws_security_group.small_node_group.id
+        ]
+        iam_role_additional_policies = [
+        "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy" 
         ]
       }
     }
-    web = {
-      medium_group = {
-        name                   = var.cluster_node_name
-        instance_types         = [var.eks_instance_type]
-        min_size               = var.cluster_min_size
-        max_size               = var.cluster_max_size
-        desired_size           = var.cluster_desired_size
-        vpc_security_group_ids = [
+    web = {      medium_group = {
+        name                         = var.cluster_node_name
+        instance_types               = [var.eks_instance_type]
+        min_size                     = var.cluster_min_size
+        max_size                     = var.cluster_max_size
+        desired_size                 = var.cluster_desired_size
+        vpc_security_group_ids       = [
           aws_security_group.small_node_group.id
+        ]
+        iam_role_additional_policies = [
+        "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy" 
         ]
       }
     }
