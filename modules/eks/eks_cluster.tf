f452d69d176cfcab10b87b8052d3b1cf0579bc65
Darwin Rinderer <darwin@helium.foundation>
2023-07-11 12:11:14 -0400

    another test

--- modules/eks/eks_cluster.tf
+++ modules/eks/eks_cluster.tf
@@ -12,11 +12,36 @@ module "eks" {
 
   enable_irsa = true
 
-  cluster_addons = {
+cluster_addons = {
+    # aws eks describe-addon-versions --addon-name coredns
+    coredns = {
+      addon_version               = "v1.8.7-eksbuild.3"
+      resolve_conflicts_on_update = "PRESERVE"
+    }
+    # aws eks describe-addon-versions --addon-name kube-proxy
+    kube-proxy = {
+      addon_version               = "v1.24.7-minimal-eksbuild.2"
+      resolve_conflicts_on_update = "PRESERVE"
+    }
+    # aws eks describe-addon-versions --addon-name vpc-cni
+    vpc-cni = {
+      addon_version               = "v1.11.4-eksbuild.1"
+      resolve_conflicts_on_update = "PRESERVE"
+      configuration_values = jsonencode({
+        initenv = {
+          DISABLE_TCP_EARLY_DEMUX = "true"
+        }
+        env = {
+          ENABLE_POD_ENI = "true"
+        }
+      })
+    }
+    # aws eks describe-addon-versions --addon-name aws-ebs-csi-driver
     aws-ebs-csi-driver = {
-      addon_version = "v1.20.0-eksbuild.1"
+      addon_version               = "v1.20.0-eksbuild.1"
     }
   }
+
 
   # Remove this tag to allow the aws lb to target a single sg using the tag
   # https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2258
