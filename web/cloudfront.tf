3480ee48086cabcce1d7627159d0302c5f9277fd
Darwin Rinderer <darwin@helium.foundation>
2023-10-10 15:44:36 -0400

    Updating data resource usage

--- web/cloudfront.tf
+++ web/cloudfront.tf
@@ -6,7 +6,7 @@ resource "aws_cloudfront_distribution" "lb_distribution" {
 
   origin {
     domain_name = data.aws_lb.lb.dns_name
-    origin_id   = data.laws_lb.b.dns_name
+    origin_id   = data.aws_lb.lb.dns_name
 
     custom_origin_config {
       http_port                = 80
@@ -21,7 +21,7 @@ resource "aws_cloudfront_distribution" "lb_distribution" {
   default_cache_behavior {
     allowed_methods          = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods           = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id         = data.lb.dns_name
+    target_origin_id         = data.aws_lb.lb.dns_name
     viewer_protocol_policy   = "redirect-to-https"
     cache_policy_id          = "658327ea-f89d-4fab-a63d-7e88639e58f6" // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-caching-optimized
     origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3" // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-origin-request-policies.html#managed-origin-request-policy-all-viewer
