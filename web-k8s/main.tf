7beb7669566a950474aa9c0a32e36ad7a338e370
Darwin Rinderer <darwin@helium.foundation>
2023-10-12 12:10:47 -0400

    Adding k8s updates

--- web-k8s/main.tf
+++ web-k8s/main.tf
@@ -93,15 +93,15 @@ resource "kubernetes_service_account" "public_monitoring_rds_monitoring_user_acc
   }
 }
 
-# resource "kubernetes_service_account" "invalidation_role" {
-#   metadata {
-#     name        = "invalidation-role"
-#     namespace   = "helium"
-#     annotations = {
-#       "eks.amazonaws.com/role-arn" = data.aws_iam_role.invalidation_role.arn,
-#     }
-#   }
-# }
+resource "kubernetes_service_account" "invalidation_role" {
+  metadata {
+    name        = "invalidation-role"
+    namespace   = "helium"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = data.aws_iam_role.invalidation_role.arn,
+    }
+  }
+}
 
 resource "kubectl_manifest" "rds-access-security-group-policy" {
     yaml_body = <<YAML
